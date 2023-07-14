84be7635c7f0c0a26c2b9ba17303a1b13a9fb226
barma <barmatey@gmail.com>
2023-07-14 12:21:12 +0300

    append DATE to enum

--- src/sheet/enums.py
+++ src/sheet/enums.py
@@ -1,13 +1,14 @@
 import enum
 import typing
 
-Dtype = typing.Literal['NUMBER', 'TEXT', 'BOOLEAN']
+Dtype = typing.Literal['NUMBER', 'TEXT', 'BOOLEAN', 'DATE', ]
 
 
 class CellDtype(enum.Enum):
     NUMBER = 'NUMBER'
     TEXT = 'TEXT'
     BOOLEAN = 'BOOLEAN'
+    DATE = 'DATE'
 
 
 CellTextAlign = typing.Literal['left', 'center', 'right']
