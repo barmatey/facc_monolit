9ddef91955ccb2e0adaa72eb00d6f2f4126f41dc
barma <barmatey@gmail.com>
2023-07-19 11:22:51 +0300

    some refactoring

--- src/report/repository.py
+++ src/report/repository.py
@@ -6,12 +6,13 @@ from pydantic import BaseModel
 
 from src import core_types
 from src import repository_postgres as postgres
-from . import entities
+from . import entities, schema
 
 Entity = typing.TypeVar(
     'Entity',
     entities.Group,
     entities.Report,
+    entities.ReportCategory,
 )
 
 
@@ -42,22 +43,28 @@ class CategoryRepo(postgres.CategoryRepo, CrudRepo):
 
 
 class GroupRepo(CrudRepo):
-    group_repo = postgres.GroupRepo
+    repo = postgres.GroupRepo
 
     async def create(self, data: entities.GroupCreate) -> Entity:
-        return await self.group_repo().create(data)
+        return await self.repo().create(data)
 
-    async def retrieve_bulk(self, filter_by: dict, order_by: core_types.OrderBy) -> list[Entity]:
-        return await self.group_repo().retrieve_bulk(filter_by, order_by)
+    async def retrieve_bulk(self, filter_by: dict, order_by: core_types.OrderBy) -> list[entities.Group]:
+        return await self.repo().retrieve_bulk(filter_by, order_by)
 
-    async def retrieve(self, filter_by: dict) -> Entity:
-        return await self.group_repo().retrieve(filter_by)
+    async def retrieve(self, filter_by: dict) -> entities.Group:
+        return await self.repo().retrieve(filter_by)
 
-    async def update(self, instance: Entity, data: core_types.DTO) -> Entity:
+    async def update(self, instance: Entity, data: core_types.DTO) -> entities.Group:
         raise NotImplemented
 
+    async def update_sheet(self, instance: Entity, data: schema.GroupSheetUpdateSchema) -> entities.Group:
+        return await self.repo().update_sheet(instance, data)
+
     async def delete(self, filter_by: dict) -> core_types.Id_:
-        return await self.group_repo().delete(filter_by)
+        return await self.repo().delete(filter_by)
+
+    async def retrieve_linked_sheet_as_dataframe(self, group_id: core_types.Id_) -> pd.DataFrame:
+        return await self.repo().retrieve_linked_sheet_as_dataframe(group_id)
 
 
 class ReportRepo(postgres.ReportRepo, CrudRepo):
