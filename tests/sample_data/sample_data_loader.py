9592333d03d96aaa8258079e1565b66fc50cfff0
barma <barmatey@gmail.com>
2023-07-30 16:11:35 +0300

    __ADDING_TESTS__ processing

--- tests/sample_data/sample_data_loader.py
+++ tests/sample_data/sample_data_loader.py
@@ -16,9 +16,17 @@ def load_source_values() -> list[dict]:
     data = data.to_dict(orient='records')
     return data
 
+
 def load_wire_values() -> list[dict]:
     path = Path(f"{BASE_PATH}/wire_table.csv")
     data = pd.read_csv(path)
     data['date'] = pd.to_datetime(data['date'])
+    data = data.to_dict(orient='records')
+    return data
+
+
+def load_sheet_values() -> list[dict]:
+    path = Path(f"{BASE_PATH}/sheet_table.csv")
+    data = pd.read_csv(path)
     data = data.to_dict(orient='records')
     return data
