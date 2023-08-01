126b801098467ebca7be9744f946484de6e6b904
barma <barmatey@gmail.com>
2023-08-01 10:13:20 +0300

    CONTROL

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from src.publisher.router import router as router_publisher
 from src.wire.router import router_wire, router_source
 from src.sheet.router import router as router_sheet
 from src.report.router import router_report, router_group, router_category
@@ -22,7 +21,6 @@ app.add_middleware(
 )
 
 
-app.include_router(router_publisher)
 app.include_router(router_source)
 app.include_router(router_wire)
 app.include_router(router_group)
