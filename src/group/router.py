242b797e64c5988b06f284b2fd284b1113299445
barma <barmatey@gmail.com>
2023-08-04 15:11:56 +0300

    processing

--- src/group/router.py
+++ src/group/router.py
@@ -18,8 +18,7 @@ router_group = APIRouter(
 @helpers.async_timeit
 async def create_group(data: events.GroupCreated, get_asession=Depends(db.get_async_session)) -> Group:
     async with get_asession as session:
-        results = await messagebus.handle(data, session)
-        group: Group = results
+        group: Group = await messagebus.handle(data, session)
         await session.commit()
         return group
 
@@ -29,8 +28,7 @@ async def create_group(data: events.GroupCreated, get_asession=Depends(db.get_as
 async def get_group(group_id: core_types.Id_, get_asession=Depends(db.get_async_session)) -> JSONResponse:
     async with get_asession as session:
         event = events.GroupGotten(group_id=group_id)
-        results = await messagebus.handle(event, session)
-        group: ExpandedGroup = results
+        group: ExpandedGroup = await messagebus.handle(event, session)
         await session.commit()
         return JSONResponse(content=group.to_json())
 
@@ -41,8 +39,7 @@ async def get_groups(category: GroupCategory = None,
                      get_asession=Depends(db.get_async_session)) -> list[Group]:
     async with get_asession as session:
         event = events.GroupListGotten()
-        result = await messagebus.handle(event, session)
-        groups: list[Group] = result
+        groups: list[Group] = await messagebus.handle(event, session)
         await session.commit()
         return groups
 
@@ -51,10 +48,10 @@ async def get_groups(category: GroupCategory = None,
 @helpers.async_timeit
 async def partial_update_group(group_id: core_types.Id_, data: events.GroupPartialUpdated,
                                get_asession=Depends(db.get_async_session)) -> Group:
-    data.id = group_id
     async with get_asession as session:
-        result = await messagebus.handle(data, session)
-        updated: Group = result
+        event = data.copy()
+        event.id = group_id
+        updated: Group = await messagebus.handle(data, session)
         await session.commit()
         return updated
 
@@ -64,7 +61,6 @@ async def partial_update_group(group_id: core_types.Id_, data: events.GroupParti
 async def delete_group(group_id: core_types.Id_, get_asession=Depends(db.get_async_session)) -> core_types.Id_:
     async with get_asession as session:
         event = events.GroupDeleted(group_id=group_id)
-        result = await messagebus.handle(event, session)
-        deleted_id: core_types.Id_ = result
+        deleted_id: core_types.Id_ = await messagebus.handle(event, session)
         await session.commit()
         return deleted_id
